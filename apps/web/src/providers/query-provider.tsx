60b355fbb3d42f81ab4b5ae47d9fece24f2324dd
shu12388y <spl.sp999@gmail.com>
2026-02-04 19:11:18 +0530

    added query provider

---
+++ apps/web/src/providers/query-provider.tsx
@@ -0,0 +1,12 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const query = new QueryClient();
+function QueryProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <QueryClientProvider client={query}>{children}</QueryClientProvider>
+    </>
+  );
+}
+
+export default QueryProvider;
